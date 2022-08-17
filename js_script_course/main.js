663572f10c1c60b21b3597a23817535858636c37
Ismail-creator <82962233+Ismail-creator@users.noreply.github.com>
2022-08-17 10:17:41 +0300

    second commit

--- js_script_course/main.js
+++ js_script_course/main.js
@@ -1,7 +1,7 @@
 const myForm = document.querySelector('#my-form');
 const nameInput = document.querySelector('#name');
 const emailInput = document.querySelector('#email');
-const msg = document.querySelector('#msg');
+const msg = document.querySelector('.msg');
 const userList = document.querySelector('#users');
 
 myForm.addEventListener('submit', onsubmit);
